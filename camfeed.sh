f4ae45556b5170875042746d32a74645bef505b3
xheen908 <xheen908@gmail.com>
2019-05-31 17:16:50 +0200

    Update camfeed.sh

--- camfeed.sh
+++ camfeed.sh
@@ -4,7 +4,7 @@ rm /etc/opkg/secret-feed.conf
 rm /etc/opkg/deb-feed.conf
 opkg remove softcam-feed-universal
 cd /tmp
-wget https://github.com/xheen908/camfeed/blob/master/softcam-feed-universal_4.3-r0_all.ipk
+wget --no-check-certificate https://github.com/xheen908/camfeed/blob/master/softcam-feed-universal_4.3-r0_all.ipk
 opkg install softcam-feed-universal_4.3-r0_all.ipk
 opkg update
 cd
